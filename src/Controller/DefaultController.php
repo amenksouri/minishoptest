1a2d88e961251c401a3f1fe0adcc7664d85397de
amenksouri <amenksouri5@gmail.com>
2024-11-08 22:04:17 +0100

    making maquette

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +10,20 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
+    #[Route('/', name: 'app_default')]
     public function index(ProductRepository $pr): Response
     {
         //dd($pr->findAll());
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/item.html.twig', [
             'controller_name' => 'DefaultController',
             'products' => $pr->findAll(),
+        ]);
+    }
+    #[Route('/item/{id}', name: 'app_item_product_show', methods: ['GET'])]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
         ]);
     }
 }
